df5522df190d6cc73639ee6221270fefe7fe29d8
Luna Ruiz <lunaruiz@Lunas-MacBook-Pro.local>
2022-01-02 16:32:18 -0500

    fixed image load problem (mapped incorrectly)

--- src/About.js
+++ src/About.js
@@ -8,64 +8,45 @@ function About() {
     const importAllFrom = (dir) => {
         return dir.keys().map(dir);
     }
-
-    // determine if page is loading or not
-    const onPageLoad = () => {
-        window.onload = (e) => {
-            setLoading(false);
-        }
-    }
-
-    const [loading, setLoading] = useState(true);
+    
     const personalImgs = importAllFrom(require.context('./img/personal', false, /\.(png|jpe?g|svg)$/));
 
-    useEffect(() => {
-        setLoading(true);
-        onPageLoad();
-    }, []);
-
     return (
-        <>
-        {loading === true ? (
-            <Loading />
-        ):(
-            <div className="about">
-                <div className="about-intro">
-                    <p> I'm a <span className="important">model, actress, singer, dancer, artist, writer,</span> with 9+ years of experience </p>
-                </div>
+        <div className="about">
+            <div className="about-intro">
+                <p> I'm a <span className="important">model, actress, singer, dancer, artist, writer,</span> with 9+ years of experience </p>
+            </div>
 
-                <div className="about-bio">
-                    <div className="about-bio-left">
-                        <p>
-                            A Harlem born Latina, <span className="important">Nina Rodriguez</span> is a model and actress working predominantly in the NYC area. 
-                            She started modeling because she wanted to push a lane for woman like herself. She has worked with incredible artists of all mediums. 
-                            Shooting a large range of work, from commercial print, beauty, and editorial work. Nina is breaking barriers getting consistent work 
-                            with brands such as Champion, L'Oréal, Samsung, Makeup Forever, and SteadfastMagazine to name a few. When she's not working she's 
-                            being creative in other ways, whether it's painting, or writing.
-                        </p>
-                        <div className="about-bio-left-nums">
-                            <p> <span className="important num">45+</span> Clients</p>
-                            <p> <span className="important num">100+</span> Projects</p>
-                            <p> <span className="important num">25+</span> Collabs</p>
-                        </div>
-                    </div>
-
-                    <div className="about-bio-right">
-                        <img src={bioImg} alt="Nina Rodriguez" />
-                        <div className="about-bio-right-border"></div>
+            <div className="about-bio">
+                <div className="about-bio-left">
+                    <p>
+                        A Harlem born Latina, <span className="important">Nina Rodriguez</span> is a model and actress working predominantly in the NYC area. 
+                        She started modeling because she wanted to push a lane for woman like herself. She has worked with incredible artists of all mediums. 
+                        Shooting a large range of work, from commercial print, beauty, and editorial work. Nina is breaking barriers getting consistent work 
+                        with brands such as Champion, L'Oréal, Samsung, Makeup Forever, and SteadfastMagazine to name a few. When she's not working she's 
+                        being creative in other ways, whether it's painting, or writing.
+                    </p>
+                    <div className="about-bio-left-nums">
+                        <p> <span className="important num">45+</span> Clients</p>
+                        <p> <span className="important num">100+</span> Projects</p>
+                        <p> <span className="important num">25+</span> Collabs</p>
                     </div>
                 </div>
 
-                <div className="border"></div>
-
-                <div className="about-body">
-                    {personalImgs.map((img, idx) =>
-                        <img key={idx} src={img.default} alt="personal" loading="lazy" />
-                    )}
+                <div className="about-bio-right">
+                    <img src={bioImg} alt="Nina Rodriguez" />
+                    <div className="about-bio-right-border"></div>
                 </div>
             </div>
-        )}
-        </>
+
+            <div className="border"></div>
+
+            <div className="about-body">
+                {personalImgs.map((img, idx) =>
+                    <img key={idx} src={img} alt="personal" loading="lazy" />
+                )}
+            </div>
+        </div>
     );
 }
 
--- src/Work.js
+++ src/Work.js
@@ -7,34 +7,14 @@ function Work() {
     const importAllFrom = (dir) => {
         return dir.keys().map(dir);
     }
-
-    // determine if page is loading or not
-    const onPageLoad = () => {
-        window.onload = (e) => {
-            setLoading(false);
-        }
-    }
-
-    const [loading, setLoading] = useState(true);
     const workImgs = importAllFrom(require.context('./img/work', false, /\.(png|jpe?g|svg)$/));
 
-    useEffect(() => {
-        setLoading(true);
-        onPageLoad();
-    }, []);
-
     return (
-        <>
-        {loading === true ? (
-            <Loading />
-        ):(
-            <div className="work">
-                {workImgs.map((img, idx) =>
-                    <img key={idx} src={img.default} alt="work" loading="lazy" />
-                )}
-            </div>
-        )}
-        </>
+        <div className="work">
+            {workImgs.map((img, idx) =>
+                <img key={idx} src={img} alt="work" loading="lazy" />
+            )}
+        </div>
     );
 }
 
